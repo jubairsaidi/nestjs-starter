69a5d897f418ba2998deeaf0115cd76382ecc0a8
Jubair Saidi <jubair.saidi@gmail.com>
2019-10-30 23:31:18 -0400

    Adds status endpoint.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+export interface StatusResponse {
+  serviceName: string;
+  gitHash: string;
+  version: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getStatus(): StatusResponse {
+    return {
+      serviceName: process.env.npm_package_name,
+      gitHash: process.env.npm_package_gitHead,
+      version: process.env.npm_package_version
+    };
   }
 }
