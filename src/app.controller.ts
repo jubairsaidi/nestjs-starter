9ff7b44f951fa37a80671d5ee3f31947faf1ea53
Jubair Saidi <jubair.saidi@gmail.com>
2019-11-02 21:23:37 -0500

    playing with nested DTOs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ValidateNested
+} from 'class-validator';
 import { AppService, StatusResponse } from './app.service';
+
+export class MessageDto {
+  @IsString()
+  Some: string;
+}
+
+export class ValidateDto {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  foo: number;
+
+  @IsNotEmpty()
+  bar: string;
+
+  @ValidateNested({ each: true })
+  @Type(() => MessageDto)
+  message: MessageDto;
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +40,11 @@ export class AppController {
   @Get('status')
   getServiceStatus(): StatusResponse {
     return this.appService.getStatus();
+  }
+
+  @Post('notify')
+  postValidate(@Body() validateDto: ValidateDto): any {
+    console.log(validateDto);
+    return validateDto;
   }
 }
